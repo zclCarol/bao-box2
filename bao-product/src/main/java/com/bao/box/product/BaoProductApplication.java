8e5286200a20580f56afa6e0b1cb2264c3ff1da0
zclCarol <dabaosbox@sina.com>
2023-12-16 15:36:25 +0800

    基础电商业务流程完成 TODO 搜索，减库存

--- bao-product/src/main/java/com/bao/box/product/BaoProductApplication.java
+++ bao-product/src/main/java/com/bao/box/product/BaoProductApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class BaoProductApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BaoProductApplication.class, args);
     }
